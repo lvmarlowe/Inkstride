46f1f8cb6abaf61333b33e0b8901047d5f9c49a2
lvmarlowe <88361355+lvmarlowe@users.noreply.github.com>
2026-02-03 17:54:25 -0700

    Kotlin configuration: Gradle and build fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -35,6 +36,11 @@ android {
 
     buildFeatures {
         compose = true
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
     }
 }
 
